5dff3ea733c8a7f130a7b9fb6a7f2b7b63452bd9
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2025-09-07 11:10:45 +0100

    feat: get functions

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -37,6 +37,9 @@ pub fn router(state: AppState) -> Router {
         .route("/health", get(health::health_check))
         .route("/group", get(group::get_group).post(group::create_group))
         .route("/pay_group", post(pay_group::pay_group))
+        .route("/all_groups", get(group::get_groups))
+        // .route("/history", get(group::get_groups_metrics))
+        .route("/transfer_metrics", get(group::get_payments_totals))
         .route("/store_payment_distribution_history", post(pay_group::store_payment_distribution_history))
         .with_state(state)
 }
